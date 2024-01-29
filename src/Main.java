169b9dd86a9b8a6cb86a52f20cb02ebbc8ba4f5e
Radioactyve <radioactivex55@gmail.com>
2024-01-29 23:34:54 +0100

    3.bertsioa

--- src/Main.java
+++ src/Main.java
@@ -2,52 +2,70 @@ import weka.core.Instances;
 import weka.core.converters.ConverterUtils.DataSource;
 import weka.core.Attribute;
 import java.util.*;
+import java.io.*;
 
 
 public class Main {
 
     public static void main(String[] args) {
         try {
-            // datuak kargatu
-            DataSource source = new DataSource("C:/Users/radio/Desktop/System/Uni/3/Erabakiak/1. Praktika Datuak-20240122/heart-c.arff");
+            //Datuak hartu
+            String inPath="C:/Users/radio/Desktop/System/Uni/3/Erabakiak/1. Praktika Datuak-20240122/heart-c.arff";
+            DataSource source = new DataSource(inPath);
             Instances data = source.getDataSet();
+            //Emaitzak gorde
+            String outPath="C:/Users/radio/Desktop/emaitzak.txt";
+            FileWriter fileWriter = new FileWriter(outPath);
 
-            // 1. Aztertzen ari garen fitxategiko path-a
-            System.out.println("1- Aztertzen ari garen fitxategiko path-a: " + source.toString());
+            //1-Aztertzen ari garen fitxategiko path-a
+            fileWriter.write("1- Aztertzen ari garen fitxategiko path-a: " + inPath + "\n");
 
-            // 2. Instantzia kopurua
-            System.out.println("2- Instantzia kopurua: " + data.numInstances());
+            //2-Instantzia kopurua
+            fileWriter.write("2- Instantzia kopurua: " + data.numInstances() + "\n");
 
-            // 3. Atributu kopurua
-            System.out.println("3- Atributu kopurua: " + data.numAttributes());
+            //3-Atributu kopurua
+            fileWriter.write("3- Atributu kopurua: " + data.numAttributes() + "\n");
 
-            // 4. Lehenengo atributuak har ditzakeen balio ezberdinak (balio ezberdin kopurua)
-            System.out.println("4- Lehenengo atributuak har ditzakeen balio ezberdinak:");
-            Enumeration<Attribute> attributes = data.enumerateAttributes();
-            while (attributes.hasMoreElements()) {
-                Attribute attribute = attributes.nextElement();
-                if (attribute.index() == 0) continue; // Saltar el identificador de la instancia
-                System.out.println("   - " + attribute.name() + ": " + attribute.numValues());
+            //4-Lehenengo atributuak har ditzakeen balio ezberdinak (balio ezberdin kopurua)
+            fileWriter.write("4- Lehenengo atributuak har ditzakeen balio ezberdinak:" );
+            Attribute firstAttribute = data.attribute(0);
+            ArrayList<String> firstAtributeValues = new ArrayList<String>();
+            int valueCount = 0;
+            for (int i = 0; i < data.numInstances(); i++){
+                if (!firstAtributeValues.contains(data.get(i).stringValue(0))){
+                    firstAtributeValues.add(data.get(i).stringValue(0));
+                    valueCount++;
+                }
             }
 
-            // 5. Azken atributuak hartzen dituen balioak eta beraien maiztasuna
-            System.out.println("5- Azken atributuak hartzen dituen balioak eta beraien maiztasuna:");
+            for (String value : firstAtributeValues) {
+                fileWriter.write(value + "\n");
+            }
+            fileWriter.write(valueCount);
 
-            Attribute lastAttribute = data.attribute(data.numAttributes() - 1);
+
+            //5-Azken atributuak hartzen dituen balioak eta beraien maiztasuna
+            fileWriter.write("\n" + "5- Azken atributuak hartzen dituen balioak eta beraien maiztasuna:");
+            Attribute lastAttribute = data.attribute(data.numAttributes() -1);
+
             HashMap<String, Integer> valueFrequencyMap = new HashMap<>();
             for (int i = 0; i < data.numInstances(); i++) {
                 String value = data.instance(i).stringValue(lastAttribute);
                 valueFrequencyMap.put(value, valueFrequencyMap.getOrDefault(value, 0) + 1);
             }
             for (String value : valueFrequencyMap.keySet()) {
-                System.out.println("   - Balioa: " + value + ", Maiztasuna: " + valueFrequencyMap.get(value));
+                fileWriter.write("   - Balioa: " + value + ", Maiztasuna: " + valueFrequencyMap.get(value));
             }
 
-            // 6. Maiztasun txikien duen klasearen identifikatzailea eman
-            System.out.println("6- Maiztasun txikien duen klasearen identifikatzailea: " + getMinFrequencyClass(data));
+            //6-Maiztasun txikien duen klasearen identifikatzailea eman
+            fileWriter.write("\n" + "6- Maiztasun txikien duen klasearen identifikatzailea: " + getMinFrequencyClass(data));
 
-            // 7. Azken aurreko atributuak dituen missing value kopurua
-            System.out.println("7- Azken aurreko atributuak dituen missing value kopurua: " + countMissingValues(data, data.numAttributes() - 2));
+            //7-Azken aurreko atributuak dituen missing value kopurua
+            fileWriter.write("\n" +  "7- Azken aurreko atributuak dituen missing value kopurua: " + countMissingValues(data, data.numAttributes() - 2));
+
+            //fileWriter itxi
+            fileWriter.flush();
+            fileWriter.close();
 
         } catch (Exception e) {
             e.printStackTrace();
@@ -57,30 +75,26 @@ public class Main {
     // Maiztasun txikien duen klasearen identifikatzailea lortu
     private static String getMinFrequencyClass(Instances data) {
         Attribute classAttribute = data.attribute(data.numAttributes() - 1);
+        String minFrequencyClass = null;
+        int minFrequency = Integer.MAX_VALUE;
 
         HashMap<String, Integer> classFrequencyMap = new HashMap<>();
         for (int i = 0; i < data.numInstances(); i++) {
             String classValue = data.instance(i).stringValue(classAttribute);
-            classFrequencyMap.put(classValue, classFrequencyMap.getOrDefault(classValue, 0) + 1);
-        }
-
-        String minFrequencyClass = null;
-        int minFrequency = Integer.MAX_VALUE;
-
-        for (String value : classFrequencyMap.keySet()) {
-            int frequency = classFrequencyMap.get(value);
+            int frequency = classFrequencyMap.getOrDefault(classValue, 0) + 1;
+            classFrequencyMap.put(classValue, frequency);
             if (frequency < minFrequency) {
                 minFrequency = frequency;
-                minFrequencyClass = value;
+                minFrequencyClass = classValue;
             }
         }
         return minFrequencyClass;
     }
 
+
     // atributu baten missing value kopurua kontatu
     private static int countMissingValues(Instances data, int attributeIndex) {
         int missingValues = 0;
-
         for (int i = 0; i < data.numInstances(); i++) {
             if (data.instance(i).isMissing(attributeIndex)) {
                 missingValues++;
